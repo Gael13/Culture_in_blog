af67741e562176f441efb0624e447cf1854c245d
Gaël Rivaud <gaelrivaud@MacBook-Air-de-Gael.local>
2021-11-19 14:43:49 +0100

    Add Comment model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  Article.create!(title: "Article #{i}", content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
+end
